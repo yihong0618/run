3dfa2ae95432fecbf179aebae03bb3b0a90b506c
yihong0618 <zouzou0208@gmail.com>
2020-09-30 11:44:46 +0800

    delete: some useless code

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,7 +1,8 @@
 // const
 const MAPBOX_TOKEN = '';
 const MUNICIPALITY_CITIES_ARR = ['北京市', '上海市', '天津市', '重庆市', '香港特别行政区', '澳门特别行政区'];
+const IS_CHINESE = true
 
 export {
-  MAPBOX_TOKEN, MUNICIPALITY_CITIES_ARR,
+  MAPBOX_TOKEN, MUNICIPALITY_CITIES_ARR, IS_CHINESE
 };
