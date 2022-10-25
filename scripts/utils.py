cc025aa4f11474f988e50f2213bc4edfbe8a7fce
yihong0618 <zouzou0208@gmail.com>
2022-10-25 10:33:10 +0800

    fix

--- scripts/utils.py
+++ scripts/utils.py
@@ -75,7 +75,10 @@ def upload_file_to_strava(client, file_name, data_type):
             print(f"Strava API Rate Limit Exceeded. Retry after {timeout} seconds")
             print()
             time.sleep(timeout)
-            r = client.upload_activity(activity_file=f, data_type=data_type)
+            try:
+                r = client.upload_activity(activity_file=f, data_type=data_type)
+            except:
+                pass
         print(
             f"Uploading {data_type} file: {file_name} to strava, upload_id: {r.upload_id}."
         )
